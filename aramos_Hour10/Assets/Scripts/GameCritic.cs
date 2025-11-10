48a0014eab0ad5a1af29bcb350984ea992673bcd
shrimpfetti <sadsackofpeeledpotatoes@gmail.com>
2025-11-10 02:12:36 -0800

    added chaos balls

---
+++ aramos_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,23 @@
+/* 1. Is the game too easy or hard? I don't think it's either or, it seems to be purely rng based as there's nothing you can really do to compel
+ any of the balls to move towards the bumper, thus taking away the player's only form of actually interacting with the game. You kind of just 
+ sit there and fumble with the bumper a bit, realize it's futile, and continue to sit there doing nothing.
+
+   2. What would make it easier or harder? I'm not really sure, I think it's fundamentally flawed honestly. It's like trying to be a pinball
+ or pong type of game, but you have zero control over the balls and they just do whatever they want. I'm thinking they'd want to keep the
+ chaos a part of the core game itself, so maybe they could make it playable by being able to position the bumper anywhere at will as opposed to
+ just sat in the middle of the field, only being able to turn in place.
+
+   3. What would give it a "wow" factor? Maybe giving some sort of visual or audio cue ( like confetti or an explosion sound, like the Minecraft
+ firework sound effect) that one of the balls entered their linked goal, considering that I just tested the game again and was like "oh what where 
+ did one of my goal lights go?" And then remembered oh yeah that's what happens when the ball scores a goal. 
+
+   4. What parts of the game are fun? What parts of the game are tedious? I like the lights a lot, I get that they serve an actual core purpose to
+ show when you still have goals left to score, but they are so fun to me. As for the tedious thing, I think the whole thing is a bit tedious. 
+ Again, you're sitting there waiting to win, which can take anywhere from instantly to forever.
+
+Suggestions implemented:
+Changed the size of the player's bumper (scaled it up along the x axis)
+Added a chaos goal
+Covered up the outside water with terrain
+Added variations of the chaos balls (made new faster and slower ones, I wasn't sure what "variations" entailed so I did the first thing I thought of)
+ */
