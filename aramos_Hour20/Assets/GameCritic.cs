84c9a4d9934c5db5b3756505ad9fce4ce21f13f4
shrimpfetti <sadsackofpeeledpotatoes@gmail.com>
2025-12-14 23:31:58 -0800

    added gauntlet runner

---
+++ aramos_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,23 @@
+/*
+1. Is the game too easy or hard? I think the difficulty is pretty in the middle.
+I actually really liked that some of the powerups were not easy to obtain, and it
+was more optimal to instead avoid them if they were too close to obstacles.
+
+2. What changes would you do to make it easier or harder? I don't think I'd change
+anything but I guess to make it a little harder there could be a secondary punishment
+for hitting any obstacles, like the inclusion of a health bar and making the player's
+health go down for every hit, or maybe a condition like in Subway Surfers where you're
+not allowed to trip(??)/run into walls from the side twice or it results in a game over.
+
+3. What would make the game more appealing? The art direction looks like it's going for
+a sci-fi sort of vibe with the laser walls, so maybe more of that + music to match said
+vibe. 
+
+4. What parts of the game are fun? I like that the movement isn't really locked to
+strictly just three lanes like most running games do, instead it's a lot more free. 
+
+5. What parts of the game are tedious? It's not necessarily tedious, I think it just
+makes the game harder which... is a good thing, but the fact that you can't get every
+powerup. The only other complaint I would have is that it takes too long to speed up.
+I personally like fast paced games so I'd prefer if it goes faster.
+*/
---
+++ aramos_Hour20/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public TextureScroller ground;
+    public float gameTime = 10;
+
+    float totalTimeElapsed = 0;
+    bool isGameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver)
+            return;
+
+        totalTimeElapsed += Time.deltaTime;
+        gameTime -= Time.deltaTime;
+
+        if (gameTime <= 0)
+            isGameOver = true;
+    }
+
+    public void AdjustTime(float amount)
+    {
+        gameTime += amount;
+        if (amount < 0)
+            SlowWorldDown();
+    }
+
+    void SlowWorldDown()
+    {
+        CancelInvoke();
+        Time.timeScale = 0.5f;
+        Invoke("SpeedWorldUp", 1);
+    }
+
+    void SpeedWorldUp()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void OnGUI()
+    {
+        if (!isGameOver)
+        {
+            Rect boxRect = new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50);
+            GUI.Box(boxRect, "Time Remaining");
+
+            Rect labelRect = new Rect(Screen.width / 2 - 60, Screen.height / 2 - 100, 120, 50);
+            GUI.Box(boxRect, "Game Over");
+        }
+
+        else
+        {
+
+            Rect labelRect = new Rect(Screen.width / 2 - 55, Screen.height / 2 - 80, 90, 40);
+            GUI.Label(labelRect, "Total Time: " + (int)totalTimeElapsed);
+
+            Time.timeScale = 0;
+        }
+    }
+}
