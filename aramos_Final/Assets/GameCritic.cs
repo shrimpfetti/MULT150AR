872a19db47ffe4aa46dd1928b814aad7718d6cd5
shrimpfetti <sadsackofpeeledpotatoes@gmail.com>
2025-12-19 22:03:29 -0800

    added final

---
+++ aramos_Final/Assets/GameCritic.cs
@@ -0,0 +1,38 @@
+/*
+1. Is the game too easy or hard? I think the difficulty is pretty in the middle.
+I actually really liked that some of the powerups were not easy to obtain, and it
+was more optimal to instead avoid them if they were too close to obstacles.
+
+2. What changes would you do to make it easier or harder? I don't think I'd change
+anything but I guess to make it a little harder there could be a secondary punishment
+for hitting any obstacles, like the inclusion of a health bar and making the player's
+health go down for every hit, or maybe a condition like in Subway Surfers where you're
+not allowed to trip(??)/run into walls from the side twice or it results in a game over.
+
+3. What would make the game more appealing? The art direction looks like it's going for
+a sci-fi sort of vibe with the laser walls, so maybe more of that + music to match said
+vibe. 
+
+4. What parts of the game are fun? I like that the movement isn't really locked to
+strictly just three lanes like most running games do, instead it's a lot more free. 
+
+5. What parts of the game are tedious? It's not necessarily tedious, I think it just
+makes the game harder which... is a good thing, but the fact that you can't get every
+powerup. The only other complaint I would have is that it takes too long to speed up.
+I personally like fast paced games so I'd prefer if it goes faster.
+
+Asset Changes:
+1. Modify the obstacle prefab
+        *Turned the obstacle into spikes
+        *Imported a new texture and applied it to the obstacle
+        *Also I made it glow like a cool red and cyan color because, as mentioned
+        before, I thought it'd be cool to match the sci-fi vibe of the rest
+        of the game! yaaay! The textures are mine btw yaaas
+
+2. Modify the powerup prefab
+        *Turned the model into a battery to fit with the sci-fi vibe
+        *The particle system was making me rip my hair out, I don't know why
+        it's not lining up with the battery correctly but WHATEVER! Also I
+        gave it the same textures as the spikes, not because I was too lazy
+        to give it new textures, but because I thought it looked cool.
+*/
---
+++ aramos_Final/Assets/Modifications.cs
@@ -0,0 +1,59 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: to fit the vibe, modified to be a bright cyan with an intensity of 2.75
+
+        2 - fog
+        modifications: made it look like the area in front of the player is further than it actually is with a darker blue color
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: more vibes. Made the sky tint darker cyan and the ground black so it looks like a void, messed around with the settings until I got something I liked
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: Light used to also be cyan, but there was way too much cyan so I changed it to red for more contrast, increased intensity to help with visibilty as well
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: I really liked the battery :( but I made a totally real dog comprised of 9 shapes (not including the empty object)
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary] yeeep
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: Assets/Materials/ThatTimeIGotIntoCrocheting.jpg
+        texture source 2: Assets/Materials/ThatTimeIGotIntoCrochetingPart2.jpg
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: Emission
+        parameter 2: Shape
+        parameter 3: Renderer
+        parameter 4: I tried messing with the changing color over lifetime thing but I'm not sure if it works 
+            UPDATE. I was working on the donut and I don't even know what happened. I'm ripping my hair out I'm losing my mind the dog got deleted somewhere along the way and I have had to remake it five times now. It's not working like it did before I'M SO DONE. For the sake of having the game even work SOMEWHAT I'm changing back to the battery
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
